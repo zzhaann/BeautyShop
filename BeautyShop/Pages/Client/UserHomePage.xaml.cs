47786b1e4ff6d3c8a50b6cc87360c04e0ca78c0a
zzhaann <zhhaann@mail.ru>
2025-04-27 20:48:15 +0500

    хоумпейж

--- BeautyShop/Pages/Client/UserHomePage.xaml.cs
+++ BeautyShop/Pages/Client/UserHomePage.xaml.cs
@@ -1,13 +1,38 @@
+using System.Threading; 
+
 namespace BeautyShop.Pages;
 
 public partial class UserHomePage : ContentPage
 {
+    private CancellationTokenSource _carouselCts;
+
     public UserHomePage()
     {
         InitializeComponent();
+        StartCarouselAutoScroll();
     }
 
+    private async void StartCarouselAutoScroll()
+    {
+        _carouselCts = new CancellationTokenSource();
+        var token = _carouselCts.Token;
 
+        while (!token.IsCancellationRequested)
+        {
+            await Task.Delay(3000);
+
+            if (PromoCarousel.ItemsSource is System.Collections.IList items && items.Count > 0)
+            {
+                PromoCarousel.Position = (PromoCarousel.Position + 1) % items.Count;
+            }
+        }
+    }
+
+    protected override void OnDisappearing()
+    {
+        base.OnDisappearing();
+        _carouselCts?.Cancel();
+    }
 
     private async void OnViewServicesClicked(object sender, EventArgs e)
     {
@@ -24,20 +49,15 @@ public partial class UserHomePage : ContentPage
         await Shell.Current.GoToAsync("CartPage");
     }
 
-
     private async void OnHistoryClicked(object sender, EventArgs e)
     {
         await Shell.Current.GoToAsync("OrderHistoryPage");
     }
 
-
-
     private async void OnLogoutClicked(object sender, EventArgs e)
     {
-
         Preferences.Remove("user_name");
         Preferences.Remove("user_role");
-
 
         await Shell.Current.GoToAsync("//WelcomePage");
     }
